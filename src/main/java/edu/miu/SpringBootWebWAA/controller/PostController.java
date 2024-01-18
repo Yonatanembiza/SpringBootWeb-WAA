f57c84552f9723dabcf79334e6364acd28a9e05b
Yonatan Embiza Tadesse <enyonathan@gmail.com>
2024-01-17 22:46:10 -0600

    second commit

--- src/main/java/edu/miu/SpringBootWebWAA/controller/PostController.java
+++ src/main/java/edu/miu/SpringBootWebWAA/controller/PostController.java
@@ -3,10 +3,7 @@ package edu.miu.SpringBootWebWAA.controller;
 import edu.miu.SpringBootWebWAA.entity.Post;
 import edu.miu.SpringBootWebWAA.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +19,17 @@ public class PostController {
     @GetMapping("/{id}")
     public Post findById(@PathVariable("id") int id){
         return postService.findById(id);
+    }
+    @DeleteMapping("/{id}")
+    public Post deleteById(@PathVariable("id") int id){
+        return postService.delete(id);
+    }
+    @PostMapping
+    public Post save(@RequestBody Post p){
+        return postService.save(p);
+    }
+    @PutMapping("/{id}")
+    public Post update(@PathVariable("id") int id, @RequestBody Post p){
+        return postService.update(id, p);
     }
 }
